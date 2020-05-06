24d0827846dc314a704bee0f8e798c52ff30f8ca
James da Silva <jamesodas@gmail.com>
2020-05-06 09:08:22 -0300

    refactor(questions): extrai casos de uso dos controllers

--- server/questions/questions-controller.js
+++ server/questions/questions-controller.js
@@ -1,102 +1,82 @@
 const Question = require('./question-model').Question;
 
-const createQuestion = require('./questions-user-case').createQuestion;
-const questionIsValid = require('./questions-user-case').questionIsValid;
-const questionHasId = require('./questions-user-case').questionHasId;
+const { createQuestion, queryQuestions } = require('./questions-user-case');
 
-const questionsController = {};
+const questionsController = { };
 
-questionsController.post = (req, res) => {
+questionsController.post = async (req, res) => {
   try {
-    if(!questionIsValid(req.body)) throw "O campo 'text' é obrigatório";
-    const question = new Question(createQuestion(req.body));
-    question.save().then((newQuestion) => {
-      return res.status(200).json({
-        success: true,
-        data: newQuestion,
-      });
+    res.status(200).json({
+      success: true,
+      data: await createQuestion(req.body, Question)
     });
   } catch (error) {
-    return res.status(500).json({
+    res.status(500).json({
       success: false,
       message: error
     });
   }
 }
 
-questionsController.get = (req, res) => {
+questionsController.get = async (req, res) => {
   try {
-    Question
-    .findById(req.params.id)
-    .then((question) => {
-    question.answers.reverse(); 
-    return res.status(200).json({
+    res.status(200).json({
       success: true,
-      data: question,
+      data: await Question
+        .findById(req.params.id)
+        .then((question) => {
+          question.answers.reverse();
+          return question;
+        })
     });
-  })
   } catch (err) {
-    return res.status(500).json({
+    res.status(500).json({
+      success: false,
       message: err
     });
   };
 }
 
-questionsController.put = (req, res) => {
-  Question.findByIdAndUpdate(req.params.id, req.body, { new: true }).then((question) => {
-    return res.status(200).json({
+questionsController.put = async (req, res) => {
+  try {
+    res.status(200).json({
       success: true,
-      data: question
+      data: await Question.findByIdAndUpdate(req.params.id, req.body, { new: true })
     });
-  }).catch((err) => {
-    return res.status(500).json({
-      message: err
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error
     });
-  });
+  }
 }
 
-questionsController.delete = (req, res) => {
-  Question.findByIdAndDelete(req.params.id).then((question) => {
-    return res.status(200).json({
+questionsController.delete = async (req, res) => {
+  try {
+    res.status(200).json({
       success: true,
-      data: question
+      data: await Question.findByIdAndDelete(req.params.id)
     });
-  }).catch((err) => {
-    return res.status(500).json({
-      message: err
+  } catch (error) {
+    res.status(500).json({
+      success: true,
+      message: error
     });
-  });
+  }
 }
 
-questionsController.getAll = (req, res) => {
-  const { q, _sort, _order, _limit, _page} = req.query;
-
-  const query = q ? { "text": { $regex: new RegExp(q), $options: 'i' } } : { };
-  const sort = _sort ? _sort : 'creationDate';
-  const order = _order ? _order : 'desc';
-  const ordination = {};
-  ordination[sort] = order;
-  const limit = _limit ? Math.abs(_limit) : 2;
-  const page = _page ? Math.abs(_page) - 1 : 0;
-
-  Question.countDocuments(query, (err, count) => {
-    Question
-      .find( query )
-      .sort( ordination )
-      .limit( limit )
-      .skip( limit * page )
-      .then((questions) => {
-      return res.status(200).json({
-        success: true,
-        length: count,
-        data: questions
-      });
-    }).catch((err) => {
-      return res.status(500).json({
-        message: err
-      });
+questionsController.getAll = async (req, res) => {
+  try {
+    res.status(200).json({
+      success: true,
+      length: await Question.count({}),
+      data: await queryQuestions(req.query, Question)
     });
-  })
+  } catch (error) {
+    res.status(500).json({
+      message: error
+    });
+  }
 }
 
 module.exports = questionsController;
--- server/questions/questions-user-case.js
+++ server/questions/questions-user-case.js
@@ -1,17 +1,34 @@
-const createQuestion = (questionAtts) => {
+const createQuestion = async (questionAtts, Question) => {
   const _question = { };
   _question.text = questionAtts.text ? questionAtts.text : '';
   _question.user = questionAtts.user ? questionAtts.user : 'anonymous';
   _question.likesCount = 0;
   _question.creationDate = new Date();
 
-  return _question;
+  if(!questionAtts.text) throw "O campo 'text' é obrigatório";
+  const question = new Question(_question);
+  question.save();
+
+  return question;
 }
 
-const questionIsValid = (questionAtts) => !!questionAtts.text;
+const queryQuestions = async (params, Question) => {
+  const { q, _sort, _order, _limit, _page} = params;
 
-const questionHasId = (params) => !!params.id;
+  const query = q ? { "text": { $regex: new RegExp(q), $options: 'i' } } : { };
+  const sort = _sort ? _sort : 'creationDate';
+  const order = _order ? _order : 'desc';
+  const ordination = {};
+  ordination[sort] = order;
+  const limit = _limit ? Math.abs(_limit) : 2;
+  const page = _page ? Math.abs(_page) - 1 : 0;
+
+  return Question
+    .find( query )
+    .sort( ordination )
+    .limit( limit )
+    .skip( limit * page );
+}
 
 module.exports.createQuestion = createQuestion;
-module.exports.questionIsValid = questionIsValid;
-module.exports.questionHasId = questionHasId;
+module.exports.queryQuestions = queryQuestions;
